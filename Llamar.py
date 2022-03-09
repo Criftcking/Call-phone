55851a5b294b66fdd5af541dbc973e7c0ba2097b
Criftcking <86767725+Criftcking@users.noreply.github.com>
2022-03-09 19:27:32 -0400

    Update Llamar.py

--- Llamar.py
+++ Llamar.py
@@ -5,15 +5,14 @@ from colorama import Fore
 from colorama import Style
 
 print("")
-print("")                                                                 print(Fore.RED + "By:Criftcking")
-print(Fore.YELLOW + "▒█░░░ █░░ █▀▀█ █▀▄▀█ █▀▀█ █▀▀▄ █▀▀█ █▀▀ ")
+print("")
+print(Fore.RED + "By:Criftcking")
+print(Fore.YELLOW + "▒█░░░ █░░ █▀▀█ █▀▄▀█ █▀▀█ █▀▀▄ █▀▀█ █▀▀")
 print(Fore.YELLOW + "▒█░░░ █░░ █▄▄█ █░▀░█ █▄▄█ █░░█ █▄▄█ ▀▀█")
 print(Fore.YELLOW + "▒█▄▄█ ▀▀▀ ▀░░▀ ▀░░░▀ ▀░░▀ ▀▀▀░ ▀░░▀ ▀▀▀")
 
-
-
-print(Fore.BLUE + "")
-numero = input("[+]Numero:")
+print(Fore.BLUE + "")                                               
+numero = input("[+]Numero:")                                              
 print("\u001b[32;1m[+]Llamando...")
 time.sleep(2)
 subprocess.call("termux-telephony-call {}".format(numero),shell=True )
