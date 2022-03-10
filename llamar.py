fb6549cadf10028661876fd6bd37222f8ffcf4d1
Criftcking <86767725+Criftcking@users.noreply.github.com>
2022-03-10 00:58:25 -0400

    Update llamar.py

--- llamar.py
+++ llamar.py
@@ -3,9 +3,36 @@ import subprocess
 import colorama
 from colorama import Fore
 from colorama import Style
+from os import system, execl
 
+system("clear")
+print("Ejecuta primero el instalador con el comando 'bash instalar.sh' ")
+time.sleep(4)
 print("")
+print(Forever.GREEN + "Es Importante que ejecutes dos veces para que te funcione")
+print(Fore.RED + "")
+print("[+]Activando...")
 print("")
+print(Fore.BLUE + "")
+time.sleep(0.9)
+print("       /` \/ `\_  _   ")
+print("       \     /` \/ `\ ")
+print("        '.  .\      /")
+print("          \/  '.  .'")
+print("                \/")
+time.sleep(0.5)
+print("                 /` \/ `\_  _  ")
+print("                 \     /` \/ `\ ")
+print("                  '.  .\      / ")
+print("                    \/  '.  .' ")
+print("                          \/ ")
+
+
+time.sleep(2.3)
+system("")
+system("")
+system("")
+time.sleep(1)
 print(Fore.RED + "By:Criftcking")
 print(Fore.YELLOW + "▒█░░░ █░░ █▀▀█ █▀▄▀█ █▀▀█ █▀▀▄ █▀▀█ █▀▀")
 print(Fore.YELLOW + "▒█░░░ █░░ █▄▄█ █░▀░█ █▄▄█ █░░█ █▄▄█ ▀▀█")
@@ -15,4 +42,5 @@ print(Fore.BLUE + "")
 numero = input("[+]Numero:")                                              
 print("\u001b[32;1m[+]Llamando...")
 time.sleep(2)
+subprocess.call("termux-telephony-call {}".format(numero),shell=True )
 subprocess.call("termux-telephony-call {}".format(numero),shell=True )
