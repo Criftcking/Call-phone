8bae119b9f5d01fb83aeb682e0a6b5ac18bebd60
Criftcking <86767725+Criftcking@users.noreply.github.com>
2022-03-10 01:11:58 -0400

    Create instalar.sh

---
+++ instalar.sh
@@ -0,0 +1,13 @@
+apt install python
+
+apt install termux-api
+
+pip install colorama
+
+
+echo -e "\e[1;33mInstala La Aplicacion Llamada 'Termux Api' desde la Play Store o desde F-Droid para que te funcione y luego Ejecuta 'python llamar.py'\e[0m"
+echo
+echo
+echo -e "\e[1;34men 10 segundos se ejecutara el programa automaticamente\e[0m"
+sleep 10
+python llamar.py
